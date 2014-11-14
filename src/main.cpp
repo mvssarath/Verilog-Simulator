834128d0b358fd6482448adffaf6939725aa3abb
Venkata Sai Sarath Marepalli <vmarepal@ece.iit.edu>
2014-11-14 14:28:23 -0600

    project 3.1 final release

--- src/main.cpp
+++ src/main.cpp
@@ -1,47 +1,79 @@
-#include "net.h"
+#include "netlist_creation.h"
 
-
-int main(int argc, char *argv[]) 
+int main(int argc, char *argv[])
 {
-
-///////////Checking the presemce of file before starting of compiling
-  if (argc < 2) 
-  {
-    std::cerr << "You should provide a file name." << std::endl;
-    return -1;
-  }
-//////////////initializing wires, components, modules classes with objects
-  	evl_wires wires;
-	evl_components components;
-	evl_ports ports;
-	evl_modules_map modules_map;
-
-////////////////////// all the project 2 code has been moved to parse_evl_file
-
-	if (!parse_evl_file(argv[1], wires, components, ports))
+//////////////////////////////COMMON FOR ALL PROJECTS///////////////////////////////////
+	if (argc < 2)
 	{
-		return false;
+		std::cerr << "You should provide a file name." << std::endl;
+		return -1;
+	}
+	std::string evl_file=argv[1];
+////////////////////////////MAIN FOR LEXICAL ANALYSIS//////////////////////////////////
+	evl_tokens tokens;
+	if (!extract_tokens_from_file(evl_file, tokens))
+	{
+		return -1;
+	}
+	if(!store_tokens_to_file(evl_file+".tokens",tokens))
+	{
+		return -1;
+	}
+	////////////////////////////MAIN FOR SYNTACTICAL ANALYSIS//////////////////////////////////
+	evl_statements statements;
+	if(!group_tokens_into_statements(statements,tokens))
+	{
+		return -1;
+	}
+	if(!store_statements_to_file(evl_file+".statements",statements))
+	{
+		return -1;
 	}
 
- 
-  std::string output_file_ = argv[1];
-  output_file_ += ".netlist";
-  std::ofstream output_file(output_file_.c_str());
-  if (!output_file) 
-  {
-    std::cerr << "I can't write " << output_file_ << "." << std::endl;
-    return -1;
-  }
-netlist netlist_1;
-if (!netlist_1.create(wires,components))
-  {
-	  return -1;
-  }
-netlist_1.netlist_write (output_file, components);
-std::cout << "Successfully wrote the following files: " << std::endl;
-	std::cout << output_file_ + ".tokens" << std::endl;
-	std::cout << output_file_ + ".statements" << std::endl;
-	std::cout << output_file_ + ".syntax" << std::endl;
-	std::cout << output_file_ + ".netlist" << std::endl;
+	evl_wires wires;
+	evl_components components;
+	evl_modules modules;
+	std::ofstream output_file((evl_file+ ".syntax").c_str());
+for (evl_statements::iterator it=statements.begin();it!= statements.end(); ++it)
+	{
+        if ((*it).type == evl_statement::MODULE)
+		{
+			if(!process_module_statement(modules,(*it)))
+			{
+				return -1;
+			}
+		}
+		else if ((*it).type == evl_statement::WIRE)
+		{
+			if(!process_wire_statement(wires,(*it)))
+			{
+				return -1;
+			}
+		}
+
+		else if ((*it).type == evl_statement::COMPONENT)
+		{
+
+            if(!process_component_statement(components,(*it)))
+			{
+				return -1;
+
+			}
+		}
+
+		else
+		{
+			break ;
+
+		}
+	}
+////////////////////////////MAIN FOR NETLIST CREATION//////////////////////////////////
+	evl_wires_table wires_table = make_wires_table(wires);
+	netlist nl;
+	if (!nl.create(wires, components, wires_table))
+	return -1;
+	std::string nl_file = std::string(argv[1])+".netlist";
+		nl.save(nl_file,modules); 
+	return 0;
 }
 
