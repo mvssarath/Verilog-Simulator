7a58d18ca65b08bbbc28863600993674003a8cdc
Venkata Sai Sarath Marepalli <vmarepal@ece.iit.edu>
2014-12-08 20:53:41 -0600

    comp_final

--- src/syntactical_analysis.cpp
+++ src/syntactical_analysis.cpp
@@ -111,7 +111,7 @@ bool store_statements_to_file(std::string file_name, const evl_statements &state
 	std::ofstream output_file(file_name.c_str());
 	if (!output_file)
 	{
-		//std::cerr << "I can't write"  output_file << "." << std::endl;
+		
 		return false;
 	}
 	display_statements(output_file, statements);
